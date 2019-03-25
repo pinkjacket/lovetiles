dcb921c2b7debb604dab01cf5b77eed81bbcdc49
James Donlan <jacketlizard@live.com>
2019-03-25 16:50:46 -0700

    draw one row

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
 function love.load()
   tilemap = {1, 0, 0, 1, 1, 0, 1, 1, 1, 0}
 end
+
+function love.draw()
+  --ipairs let you loop through a table easily
+  --i is the loop iteration, v is the value at position i
+  for i,v in ipairs(tilemap) do
+    if v == 1 then
+      love.graphics.rectangle("fill", i * 25, 100, 25, 25)
+    end
+  end
+end
