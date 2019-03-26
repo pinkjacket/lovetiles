95b85ce6b17775d75a50c0f1ed12a30671a986e2
James Donlan <jacketlizard@live.com>
2019-03-25 17:55:57 -0700

    tiles now colored via second table

--- main.lua
+++ main.lua
@@ -5,7 +5,14 @@ function love.load()
     {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
     {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-    }
+  }
+  colors = {
+    {255, 255, 255},
+    {255, 0, 0},
+    {255, 0, 255},
+    {0, 0, 255},
+    {0, 255, 255}
+  }
 end
 
 function love.draw()
@@ -14,18 +21,7 @@ function love.draw()
       --is tile not zero?
       if tile ~= 0 then
         --set the color based on the tile number
-        if tile == 1 then
-          --setColor uses RGB, optional A
-          love.graphics.setColor(255, 255, 255)
-        elseif tile == 2 then
-          love.graphics.setColor(255, 0, 0)
-        elseif tile == 3 then
-          love.graphics.setColor(255, 0, 255)
-        elseif tile == 4 then
-          love.graphics.setColor(0, 0, 255)
-        elseif tile == 5 then
-          love.graphics.setColor(0, 255, 255)
-        end
+        love.graphics.setColor(colors[tile])
         --draw the tile
         love.graphics.rectangle("fill", j * 25, i * 25, 25, 25)
       end
