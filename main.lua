47db0699be36a74f8392ceb4707a7fbf3b524f56
James Donlan <jacketlizard@live.com>
2019-03-25 17:15:50 -0700

    loops refactored with ipairs

--- main.lua
+++ main.lua
@@ -9,14 +9,9 @@ function love.load()
 end
 
 function love.draw()
-  --done without ipairs
-  --for i=1 through the number of values in tilemap
-  for i=1,#tilemap do
-    --for j through the number of values in the row
-    for j=1,#tilemap[i] do
-      --if the value at row i column j is 1
-      if tilemap[i][j] == 1 then
-        --draw rectangle
+  for i,row in ipairs(tilemap) do
+    for j,tile in ipairs(row) do
+      if tile == 1 then
         love.graphics.rectangle("fill", j * 25, i * 25, 25, 25)
       end
     end
