3d51f88fa786c11ca75cbc21f66d55ebfcff8a44
James Donlan <jacketlizard@live.com>
2019-03-25 17:37:08 -0700

    tiles now colored via branching statements

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 function love.load()
   tilemap = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 1, 1, 1, 1, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     }
 end
@@ -11,7 +11,22 @@ end
 function love.draw()
   for i,row in ipairs(tilemap) do
     for j,tile in ipairs(row) do
-      if tile == 1 then
+      --is tile not zero?
+      if tile ~= 0 then
+        --set the color based on the tile number
+        if tile == 1 then
+          --setColor uses RGB, optional A
+          love.graphics.setColor(255, 255, 255)
+        elseif tile == 2 then
+          love.graphics.setColor(255, 0, 0)
+        elseif tile == 3 then
+          love.graphics.setColor(255, 0, 255)
+        elseif tile == 4 then
+          love.graphics.setColor(0, 0, 255)
+        elseif tile == 5 then
+          love.graphics.setColor(0, 255, 255)
+        end
+        --draw the tile
         love.graphics.rectangle("fill", j * 25, i * 25, 25, 25)
       end
     end
