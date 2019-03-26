ed8a0c162eb7331908a164bada6a66c5d9a7ed19
James Donlan <jacketlizard@live.com>
2019-03-25 18:41:50 -0700

    Create more elaborate tilesets with quads

--- main.lua
+++ main.lua
@@ -1,14 +1,27 @@
 function love.load()
-  image = love.graphics.newImage("img/tile.png")
-  width = image:getWidth()
-  height = image:getHeight()
+  image = love.graphics.newImage("img/tileset.png")
+  local image_width = image: getWidth()
+  local image_height = image:getHeight()
+  width = (image_width/3) - 2 
+  height = (image_height/2) -2
+  
+  quads = {}
+  
+  for i=0,1 do
+    for j=0,2 do
+      table.insert(quads, love.graphics.newQuad(1 + j * (width + 2), 1 + i * (height + 2), width, height, image_width, image_height))
+    end
+  end
   
   tilemap = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
-    {1, 2, 3, 4, 5, 5, 4, 3, 2, 1},
-    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    {1, 6, 6, 2, 1, 6, 6, 2},
+    {3, 0, 0, 4, 5, 0, 0, 3},
+    {3, 0, 0, 0, 0, 0, 0, 3},
+    {4, 2, 0, 0, 0, 0, 1, 5},
+    {1, 5, 0, 0, 0, 0, 4, 2},
+    {3, 0, 0, 0, 0, 0, 0, 3},
+    {3, 0, 0, 1, 2, 0, 0, 3},
+    {4, 6, 6, 5, 4, 6, 6, 5}
   }
   colors = {
     {255, 255, 255},
@@ -24,10 +37,8 @@ function love.draw()
     for j,tile in ipairs(row) do
       --is tile not zero?
       if tile ~= 0 then
-        --set the color based on the tile number
-        love.graphics.setColor(colors[tile])
-        --draw the tile
-        love.graphics.draw(image, j * width, i * height)
+        --draw the right quad
+        love.graphics.draw(image, quads[tile], j * width, i * height)
       end
     end
   end
